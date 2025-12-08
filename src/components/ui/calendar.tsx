7370184cc466c785b77d3d3fc32d35fb04eb3c77
Sujit <sujeetissues@gmail.com>
2025-12-08 20:51:10 +0000

    delete this (_for element <Nav>_)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -24,11 +24,8 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
-        nav: "space-x-1 flex items-center",
-        nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-        ),
+        nav: "hidden",
+        nav_button: "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
